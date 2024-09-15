cb219ba2eb4de8f0d761e22dc1907d050662b833
rolandjkay <roland.j.kay@googlemail.com>
2024-09-15 17:34:06 +0100

    Remove filters and other unnecessary widgets from "shop"

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -12,79 +12,11 @@ import Icon from '../components/Icons/Icon';
 import Layout from '../components/Layout';
 import LayoutOption from '../components/LayoutOption';
 import ProductCardGrid from '../components/ProductCardGrid';
-import Button from '../components/Button';
+//import Button from '../components/Button';
 import Config from '../config.json';
 
 const ShopPage = (props) => {
   const [showFilter, setShowFilter] = useState(false);
-
-  /*
-   * LOAD PRODUCTS FROM STRIPE
-   *
-   * Probably should be in its own file.
-   * Load all the prodcuts of a certain type (?) from Stripe.
-   * We only have one type at the moment, and I don't know if/how 
-   * Stripe would support mode, but imagine we had ceramics and
-   * accessories, for example, that we wanted on different pages
-   */
-  /*
-  const { prices } = useStaticQuery(graphql`
-    query ProductPrices {
-      prices: allStripePrice(
-        filter: { active: { eq: true } }
-        sort: { unit_amount: ASC }
-      ) {
-        edges {
-          node {
-            id
-            active
-            currency
-            unit_amount
-            product {
-              id
-              name
-              description
-              images
-            }
-          }
-        }
-      }
-    }
-  `)
-
-  // Group prices by product
-  const products = {}
-  for (const { node: price } of prices.edges) {
-    const product = price.product
-    if (!products[product.id]) {
-      products[product.id] = product
-      products[product.id].prices = []
-    }
-    products[product.id].prices.push(price)
-  }
-
-  console.log(Object.keys(products).map((key) => (products[key].prices)))
-  const data = Object.values(products).map((product) => (
-    {
-      id: product.id,
-      price: product.prices.filter((px) => (px.active))[0].unit_amount,
-      price_id: product.prices.filter((px) => (px.active))[0].id,
-      alt: product.name,
-      name: product.name,
-      image: product.images.length > 0 ? product.images[0] : "/products/pdp1.jpeg",
-      meta: product.description,
-      //originalPrice: 100,
-    }
-
-       /* <ProductCard key={products[key].id} product={products[key]} /> *_/
-  ))
-
-  console.log("DATA", data)
-*/
-  /*
-   * END LOAD PRODUCTS FROM STRIPE
-   */
-
 
   useEffect(() => {
     window.addEventListener('keydown', escapeHandler);
@@ -119,6 +51,7 @@ const ShopPage = (props) => {
         />
         <Container size={'large'} spacing={'min'}>
           <div className={styles.metaContainer}>
+            {/*
             <span className={styles.itemCount}>476 items</span>
             <div className={styles.controllerContainer}>
               <div
@@ -136,7 +69,9 @@ const ShopPage = (props) => {
                 <Icon symbol={'caret'} />
               </div>
             </div>
+           */}
           </div>
+          {/*
           <CardController
             closeFilter={() => setShowFilter(false)}
             visible={showFilter}
@@ -146,20 +81,23 @@ const ShopPage = (props) => {
             <Chip name={'XS'} />
             <Chip name={'S'} />
           </div>
+          */}
           <div className={styles.productContainer}>
             <span className={styles.mobileItemCount}>476 items</span>
             <ProductCardGrid></ProductCardGrid>
           </div>
+          {/*
           <div className={styles.loadMoreContainer}>
             <span>6 of 456</span>
             <Button fullWidth level={'secondary'}>
               LOAD MORE
             </Button>
           </div>
+          */}
         </Container>
       </div>
 
-      <LayoutOption />
+      {/*<LayoutOption />*/}
     </Layout>
   );
 };
