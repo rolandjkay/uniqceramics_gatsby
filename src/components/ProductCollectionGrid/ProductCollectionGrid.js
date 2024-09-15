18920cf16b955047bfdb9adfe5803097e6580b32
rolandjkay <roland.j.kay@googlemail.com>
2024-09-15 18:22:33 +0100

    Remove "MEN"/"WOMEN" from home page

--- src/components/ProductCollectionGrid/ProductCollectionGrid.js
+++ src/components/ProductCollectionGrid/ProductCollectionGrid.js
@@ -8,28 +8,30 @@ const ProductCollectionGrid = (props) => {
     <div className={styles.root}>
       <ProductCollection
         image={'/collections/collection1.png'}
-        title={'Men'}
+        title={'Tablewear'}
         text={'SHOP NOW'}
         link={'/shop'}
       />
       <ProductCollection
         image={'/collections/collection2.png'}
-        title={'Women'}
+        title={'Vases'}
         text={'SHOP NOW'}
         link={'/shop'}
       />
       <ProductCollection
         image={'/collections/collection3.png'}
-        title={'Accessories'}
+        title={'Sculpture'}
         text={'SHOP NOW'}
         link={'/shop'}
       />
+      {/*
       <ProductCollection
         image={'/collections/collection4.png'}
         title={'Simple Cotton'}
         text={'SHOP NOW'}
         link={'/shop'}
       />
+      */}
     </div>
   );
 };
